822e373181f00deaeb4c2d63dd123157cfc8673d
maddishivashankar1-eng <maddishivashankar1@gmail.com>
2025-10-29 22:21:26 +0530

    Add files via upload

---
+++ Lab1P2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Lab1P2{
+	public static void main(String[] args){
+		//Scanner sc = new Scanner(System.in);
+		//int a = sc.nextInt();
+		//int b = sc.nextInt();
+
+		var a = 20;
+		var  b =20.5;
+		 var c= (a/b)/(a/b);
+		System.out.println(c);
+	}
+}
