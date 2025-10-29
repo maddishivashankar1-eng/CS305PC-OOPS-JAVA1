4c8a06d74cf483d5e3b136638a2cfa8569055d38
maddishivashankar1-eng <maddishivashankar1@gmail.com>
2025-10-29 22:13:12 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,59 @@
+class Lab1P1{
+	public static void main(String... args){
+		byte b = 120;
+		Byte bb = b ;
+		
+	
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.SIZE);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.TYPE);
+		System.out.println("----------- short   ----");
+		
+		short s = 123;
+		Short ss = s;
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MAX_VALUE);
+		System.out.println(Short.SIZE);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.TYPE);
+		System.out.println("----------- integer----");
+		
+		int i = 123;
+		Integer ii = i;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(ii.MAX_VALUE);
+		System.out.println(Integer.SIZE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.TYPE);
+		
+		System.out.println("--------Long---------");
+		long l = 123;
+		Long ll = l;
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(ll.MAX_VALUE);
+		System.out.println(Long.SIZE);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.TYPE);
+		
+		System.out.println("-----------float--------");
+		float f = 123;
+		Float ff = f;
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(ff.MAX_VALUE);
+		System.out.println(Float.SIZE);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.TYPE);
+		
+		System.out.println("------double ---");
+		double d = 123;
+		Double dd = d;
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(dd.MAX_VALUE);
+		System.out.println(Double.SIZE);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.TYPE);
+		
+	}
+}
+	
